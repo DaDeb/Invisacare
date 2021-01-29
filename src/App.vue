55737e729321422f66e3f06b05934b60abf9d790
DaDeb <72058994+DaDeb@users.noreply.github.com>
2021-01-29 10:26:24 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <GameCardsStack
+      :cards="visibleCards"
+      @rightE="handlerightE"
+      @leftE="handleleftE"
+      @downE="handledownE"
+      @upE="handleupE"
+      @hideCard="removeCardFromDeck"
+    />
+  </div>
+</template>
+
+<script>
+import GameCardsStack from "./components/GameCardsStack";
+
+export default {
+  name: "App",
+  components: {
+    GameCardsStack,
+  },
+
+  data() {
+    return {
+      visibleCards: [
+        "Swipe any way",
+        "The pupose of this test is to determine your eye power.",
+        "To meet the purpose of this test you need to follow the specified instructions",
+        "1. You will see an E shapped letter",
+        "2. Upon seeing this E shaped object you need to determine the direction the open ended side of the letter is facing",
+        "3. Slide the card to the direction of the open ended side",
+        "For example you see M you have to slide the card downward",
+        "Now give it a try, Which way do you slide the next card",
+        "E",
+        "Ǝ",
+        "m",
+        "ɯ",
+      ],
+    };
+  },
+  methods: {
+    handlerightE() {
+      console.log("swipe_rightE");
+    },
+    handleleftE() {
+      console.log("swipe_leftE");
+    },
+    handledownE() {
+      console.log("swipe_downE");
+    },
+    handleupE() {
+      console.log("swipe_upE");
+    },
+    removeCardFromDeck() {
+      this.visibleCards.shift();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./styles/mixins.scss";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/components/GameCardsStack.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cards">
+    <GameCard
+      v-for="(card, index) in cards"
+      :key="card"
+      :card="card"
+      :is-current="index === 0"
+      @rightE="$emit('rightE')"
+      @leftE="$emit('leftE')"
+      @downE="$emit('downE')"
+      @upE="$emit('upE')"
+      @hideCard="$emit('hideCard')"
+    />
+  </div>
+</template>
+
+<script>
+import GameCard from "@/components/GameCard";
+
+export default {
+  components: {
+    GameCard,
+  },
+
+  props: {
+    cards: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cards {
+  position: relative;
+  display: flex;
+  margin: 50px;
+  width: 300px;
+}
+</style>
